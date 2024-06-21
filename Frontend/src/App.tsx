d676fa38805aaf751aac01fea3a6a0a70b711655
Rahul Kumar <rahulkhg980@gmail.com>
2024-06-21 05:59:34 +0530

    Removed setLimit components

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import PaymentHistory from './pages/PaymentHistory';
 import SavingsGraph from './components/SavingsGraph';
-import SetLimits from './components/setLimits';
 import SpendingByCategory from './components/SpendingbyCategoryGraph';
 
 const App: React.FC = () => {
@@ -13,7 +12,6 @@ const App: React.FC = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/payment-history" element={<PaymentHistory />} />
         <Route path="/payment-history/savings" element={<SavingsGraph />} />
-        <Route path="/payment-history/limit" element={<SetLimits />} />
         <Route path="/payment-history/spendingbycategory" element={<SpendingByCategory />} />
       </Routes>
     </Router>
--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
-
-
-import SavingsGraph from '../components/SavingsGraph';
-import SetLimits from '../components/setLimits';
-import SpendingByCategory from '../components/SpendingbyCategoryGraph';
 import Appbar from '../components/Appbar';
 import Hero from '../components/Hero';
 
 const Dashboard: React.FC = () => {
- // const [customer, setCustomer] = useState<any>(null);
-
-  // useEffect(() => {
-  //   // Replace with your customer ID
-  //   const customerId = 'your-customer-id';
-  //   axios.get(`/api/customers/${customerId}`)
-  //     .then(response => {
-  //       setCustomer(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching customer data:', error);
-  //     });
-  // }, []);
-//  const customerId = '66713089826e5eb033b0af8d';
   return (
     <div className="App">
       <Appbar/>
       <Hero/>
-      <h1>Customer Savings and Expenditure</h1>
-      <SavingsGraph  />
-      <br></br>
-      <br></br>
-      <SetLimits/>
-      <SpendingByCategory/>
     </div>
   );
 };
